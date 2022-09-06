ada13702bf740d75d81d82b5e9fc370acb576370
chiarab22 <cbocc@myuax.com>
2022-09-06 18:02:09 +0200

    clicker

---
+++ clicker/controlador/Contador.java
@@ -0,0 +1,28 @@
+package clicker.controlador;
+
+import javax.swing.*;
+
+
+public class Contador {
+
+    private int contador = 0;
+    private JFrame frame;
+  
+    public Contador(JFrame frame) {
+      this.frame  = frame;
+    }
+  
+      public void incrementarContador() {
+          JPanel panel = new JPanel();
+          JButton button = new JButton("Click me!"); 
+          JLabel label = new JLabel("Clicks: " + contador); 
+          panel.add(button);
+          panel.add(label);
+          frame.getContentPane().add(panel);
+  
+        button.addActionListener(action -> {
+          contador++;
+          label.setText("Clicks: " + contador);
+        });
+      }
+}
